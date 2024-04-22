741461e37793c4ac6eefd933015f5954a6e88d76
AnassA7 <elhmamanass@gmail.com>
2024-04-22 01:14:04 +0100

    !!!!!

--- project/home/config.php
+++ project/home/config.php
@@ -12,7 +12,7 @@ $con = new mysqli($server, $username, $password, $db, 3307);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 } else {
-    echo "Connected successfully";
+    $tset   = "Connected successfully";
 }
 
 ?>
--- project/home/show_det.php
+++ project/home/show_det.php
@@ -11,6 +11,20 @@ function updateUserBalance($con, $user_id, $new_balance) {
     $stmt->execute();
     $stmt->close();
 }
+function updateAdminBalance($con, $new_admin_balance) {
+    $update_query = "UPDATE user SET money=? WHERE type_user= 'admin'";
+    $stmt = $con->prepare($update_query);
+    $stmt->bind_param("d", $new_admin_balance);
+    $stmt->execute();
+    $stmt->close();
+}
+function updateStudioBalance($con, $studio_id, $stud_balance) {
+    $update_query = "UPDATE studio SET money=? WHERE id=?";
+    $stmt = $con->prepare($update_query);
+    $stmt->bind_param("di", $stud_balance, $studio_id);
+    $stmt->execute();
+    $stmt->close();
+}
 
 // Retrieve user information from session
 if(isset($_SESSION['valid'])){
@@ -24,6 +38,7 @@ if(isset($_SESSION['valid'])){
     if($user_result->num_rows > 0){
         $user = $user_result->fetch_assoc();
         $user_money = $user['money'];
+        $user_full_name = $user['name'];
     }
 }
 
@@ -39,16 +54,45 @@ if(isset($_GET['id'])){
     if($game_result->num_rows > 0){
         $game = $game_result->fetch_assoc();
         $game_price = $game['price'];
+        $studio_id = $game['studio_id'];
+        $game_namme = $game['name'];
+        echo "$game_namme";
     }
 }
+$stud_query = "SELECT * FROM studio WHERE id=?";
+$s_stmt = $con->prepare($stud_query);
+$s_stmt->bind_param("i", $studio_id);
+$s_stmt->execute();
+$studio_result = $s_stmt->get_result();
+
+if($studio_result->num_rows > 0){
+    $studio = $studio_result->fetch_assoc();
+    $studio_balance = $studio['money'];
+}
+    $walo_var = 'admin';
+    $admin_balance_query = "SELECT money FROM user WHERE type_user=?";
+    $admin_balance_stmt = $con->prepare($admin_balance_query);
+    $admin_balance_stmt->bind_param("s", $walo_var);
+    $admin_balance_stmt->execute();
+    $admin_balance_result = $admin_balance_stmt->get_result();
+
+    if($admin_balance_result->num_rows > 0){
+        $admin_balance = $admin_balance_result->fetch_assoc();
+        $admin_money = $admin_balance['money'];
+        echo "$admin_money";
+    }
 
 // Process buying the game
 if(isset($_POST['add_to_cart'])){
         if(isset($user_money) && isset($game_price) && $user_money >= $game_price){
             // Deduct the price of the game from the user's balance
             $new_balance = $user_money - $game_price;
+            $new_admin_balance =$admin_money + ($game_price * 0.05);
+            $stud_balance =$studio_balance + ($game_price * 0.95);
             $buy_date = date('Y-m-d H:i:s');
             updateUserBalance($con, $user_id, $new_balance);
+            updateAdminBalance($con, $new_admin_balance);
+            updateStudioBalance($con, $studio_id, $stud_balance);
 
             // Add the game to the user's library
             $add_to_library_query = "INSERT INTO library (user_id, game_idgame, date) VALUES (?, ?, ?)";
@@ -56,6 +100,12 @@ if(isset($_POST['add_to_cart'])){
             $stmt->bind_param("iis", $user_id, $game_id, $buy_date);
             $stmt->execute();
             $stmt->close();
+            // Add the operation
+            $add_to_operation_query = "INSERT INTO operation (user_name, id_user, game_name, money, time) VALUES (?, ?, ?, ?, ?)";
+            $op_stmt = $con->prepare($add_to_operation_query);
+            $op_stmt->bind_param("sisss", $user_full_name, $user_id, $game_namme, $game_price, $buy_date);
+            $op_stmt->execute();
+            $op_stmt->close();
 
             // Redirect to a page indicating successful purchase
             header("Location: purchase_success.php");
